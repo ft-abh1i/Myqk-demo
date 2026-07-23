93052e6fa97d736320f1b908c3211afec6a42892
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-23 08:27:36 +0530

    Add React application entry point

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+const root = document.getElementById('root');
+if (!root) throw new Error('React root element is missing.');
+
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
